0c2d5ab6b5e42045b5ec579f6c6abcd222b53bb8
mgrusky <mgrusky@gmail.com>
2010-12-26 22:11:54 -0800

    now sessions (might) work

--- start_game.php
+++ start_game.php
@@ -1,22 +1,28 @@
 <?php
-session_start(); // start up your PHP session! 
+include ("library.php");
+$player = new Player('', 0); // Needs initialization before starting session. Why? Who knows...
+session_start(); // *lovingly* start up your PHP session! 
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html>
 	<head>
 		<title>simvest</title>
-		<?php include ("library.php");  ?>
 		<link rel="stylesheet" type="text/css" href="base.css" />
 	</head>
 	<body>
 		<?php
+		
+		if(isset($_POST['name'])) { 
+			// alternatively, we could check the session, forcing people to stick with their names.
+			
 			$player = new Player($_POST['name'], 50);
-			$_SESSION['name'] = $player->get_name();
-			$_SESSION['money'] = $player->get_money();
+			$_SESSION['player'] = $player;
+		}
+			
 		?>
-		<p>Name: <?php echo  $_SESSION['name']; ?></p>
-		<p>Money: $<?php echo $_SESSION['money'] ?></p>
+		<p>Name: <?php echo $_SESSION['player']->get_name(); ?></p>
+		<p>Money: $<?php echo $_SESSION['player']->get_money(); ?></p>
 		
 
 
