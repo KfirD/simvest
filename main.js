a74a0a5e94b65cdcaa98bbd539dd68b45499cf6f
Spencer Schoeben <spencer@netspencer.com>
2010-12-30 01:10:25 -0800

    forgot to add the new files

---
+++ main.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+	$("form").submit(function() {
+		alert(buy);
+		return false;
+	});
+	$("input[name='buy']").click(function() {
+		amount = $("input[name='amount']").val();
+		submit_form(amount, "buy");
+	});
+	$("input[name='sell']").click(function() {
+		amount = $("input[name='amount']").val();
+		submit_form(amount, "sell");
+	});
+	setInterval("update_data()", 5000);
+});
+
+function submit_form(amount, action) {
+	$.post("update_data.php", {amount:amount, action:action}, function(data) {
+		$("div.error").text(data.error);
+		$("img.big_chart").attr("src", data.big_chart);
+		$("img.little_chart").attr("src", data.little_chart);
+		$("p.current_price span").text(data.current_price);
+		$("li.money span").text(data.money);
+		$("li.stocks span").text(data.stocks);
+	}, "json");	
+}
+
+function update_data() {
+	$.getJSON("update_data.php", function(data) {
+		$("div.error").text(data.error);
+		$("img.big_chart").attr("src", data.big_chart);
+		$("img.little_chart").attr("src", data.little_chart);
+		$("p.current_price span").text(data.current_price);
+		$("li.money span").text(data.money);
+		$("li.stocks span").text(data.stocks);
+	});
+}
---
+++ update_data.php
@@ -0,0 +1,43 @@
+<?php
+include ("library.php");
+$player = new Player('', 0); // Needs initialization before starting session. Why? Who knows...
+session_start(); // *lovingly* start up your PHP session! 
+
+$player = $_SESSION['player'];
+
+$return['error'] = ""; // start with no error
+
+if($_POST['action'] == "buy") { // they clicked "buy"
+	$stocksToBuy = (int)$_POST['amount'];
+	$stockPrice = round(end($player->stockData->allData)*100)/100;
+	
+	if($stocksToBuy*$stockPrice <= $player->money) {
+		$player->money -= $stocksToBuy*$stockPrice + 1;	// subtract price + brokerage fee
+		$player->stocks += $stocksToBuy;				// give stocks
+	} else {
+		$return['error'] = "You have too little money."; // messy-looking, but temporary
+	}
+
+} else if($_POST['action'] == "sell") { // they clicked "sell"
+	$stocksToSell = (int)$_POST['amount'];
+	$stockPrice = round(end($player->stockData->allData)*100)/100;
+	
+	if($stocksToSell <= $player->stocks) {
+		$player->stocks -= $stocksToSell;					// subtract stocks
+		$player->money += $stocksToSell*$stockPrice - 1;	// give money - brokerage fee
+	} else {
+		$return['error'] = "You have too few stocks."; // again, messy-looking, but temporary
+	}
+}
+
+$player->stockData->tick();	
+
+$return['current_price'] = round(end($player->stockData->allData)*100)/100;
+$return['big_chart'] = $player->stockData->chartData();
+$return['little_chart'] = $player->stockData->chartVolume();
+$return['money'] = $player->get_money();
+$return['stocks'] = $player->stocks;
+
+echo json_encode($return);
+
+?>
