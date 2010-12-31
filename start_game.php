2bfbe18d57bb21f56d8c5ed463fd64e62660ee49
Spencer Schoeben <spencer@netspencer.com>
2010-12-31 00:32:58 -0800

    oops.... removing fb button from inside game

--- start_game.php
+++ start_game.php
@@ -48,7 +48,5 @@
 		
 	</body>
 	
-	<iframe src="http://www.facebook.com/plugins/like.php?href=http%3A%2F%2Fwww.facebook.com%2Fpages%2FCoal-App%2F151185061576632&amp;layout=button_count&amp;show_faces=false&amp;width=450&amp;action=like&amp;colorscheme=light&amp;height=21" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:450px; height:21px;" allowTransparency="true"></iframe>
-
 
 </html>
